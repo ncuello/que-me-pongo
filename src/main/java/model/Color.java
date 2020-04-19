8291992a2764e852213b095839a23e00d847832a
nanderson87 <nicolas.anderson@mercadolibre.com>
2020-04-19 13:44:51 -0300

    Crea Color

---
+++ src/main/java/model/Color.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Color {
+    private int rojo;
+    private int verde;
+    private int azul;
+
+    public Color(int rojo, int verde, int azul) {
+        this.rojo = rojo;
+        this.verde = verde;
+        this.azul = azul;
+    }
+}
