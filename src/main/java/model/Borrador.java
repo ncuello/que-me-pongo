2e4b63cc4de9ea69cdd373a75aedb9704a7bb579
ggallici <gallicig94@gmail.com>
2020-04-20 19:59:00 -0300

    separo logica creacional de Prenda en Borrador

---
+++ src/main/java/model/Borrador.java
@@ -0,0 +1,39 @@
+package model;
+
+import static java.util.Objects.requireNonNull;
+
+public class Borrador {
+	private TipoPrenda tipoPrenda;
+    private Material material;
+    private Color colorPrimario;
+    private Color colorSecundario;
+	private Trama trama;
+	
+	public Borrador(TipoPrenda tipoPrenda) {
+		this.tipoPrenda = requireNonNull(tipoPrenda, "tipo prenda es obligatorio");
+	}
+
+	public Borrador especificarMaterial(Material material) {
+		this.material = requireNonNull(material, "material es obligatorio");
+		return this;
+	}
+	
+	public Borrador especificarColorPrimario(Color colorPrimario) {
+		this.colorPrimario = requireNonNull(colorPrimario, "color primario es obligatorio");
+		return this;
+	}
+	
+	public Borrador especificarColorSecundario(Color colorSecundario) {
+		this.colorSecundario = colorSecundario;
+		return this;
+	}
+	
+	public Borrador especificarTrama(Trama trama) {
+		this.trama = trama;
+		return this;
+	}
+	
+	public Prenda crearPrenda() {
+		return new Prenda(tipoPrenda, material, colorPrimario, colorSecundario, trama);
+	}
+}
--- src/main/java/model/Prenda.java
+++ src/main/java/model/Prenda.java
@@ -1,7 +1,5 @@
 package model;
 
-import static java.util.Objects.requireNonNull;
-
 public class Prenda {
     private TipoPrenda tipoPrenda;
     private Material material;
@@ -9,15 +7,13 @@ public class Prenda {
     private Color colorSecundario;
 	private Trama trama;
 
-
     public Prenda(TipoPrenda tipoPrenda, Material material, Color colorPrimario, Color colorSecundario, Trama trama) {
-        this.tipoPrenda = requireNonNull(tipoPrenda, "tipo prenda es obligatorio");
-        this.material = requireNonNull(material, "material es obligatorio");
-        this.colorPrimario = requireNonNull(colorPrimario, "color primario es obligatorio");
+        this.tipoPrenda = tipoPrenda;
+        this.material = material;
+        this.colorPrimario = colorPrimario;
         this.colorSecundario = colorSecundario;
         this.trama = trama;
     }
-
 
     public Categoria getCategoria() {
         return this.tipoPrenda.getCategoria();
